327c98b9fdaf4ccb63f9c0e06c4ed7b1f28ccc28
Lukas Eigler-Harding <eiglerhardingl@gmail.com>
2020-02-15 15:04:47 -0500

    updating css and js folders to assets, adding primer starter pack

---
+++ packages/primer/assets/script.js
@@ -0,0 +1,13 @@
+/* This is your javascript file */
+
+/* this is a comment */
+// this is also a comment
+
+var myButton = document.getElementById("click_me");
+
+myButton.addEventListener('click', function(event){
+
+	console.log("clicked!");
+
+
+});
