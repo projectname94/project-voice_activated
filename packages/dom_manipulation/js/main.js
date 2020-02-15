bbd8d425916fc44c776671b21db77cbd60fe232d
Rahul S. Shinde <rshinde@mica.edu>
2020-02-15 11:55:31 -0500

    finishing dom manipulation js

--- packages/dom_manipulation/js/main.js
+++ packages/dom_manipulation/js/main.js
@@ -1,8 +1,32 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
-  
+  document.getElementById('button1').addEventListener('click', makeBoxesRed);
+  document.getElementById('button2').addEventListener('click', makeBoxesBlue);
+  document.getElementById('button3').addEventListener('click', makeOneBoxGreen);
+  document.getElementById('button4').addEventListener('click', resetBoxes);
 }
 
-function iSaidHello(){
+function makeBoxesRed(){
+  document.querySelectorAll('.demo_box').forEach((element)=>{
+    element.classList.remove('blue');
+    element.classList.add('red');
+  });
+}
+
+function makeBoxesBlue(){
+  document.querySelectorAll('.demo_box').forEach((element)=>{
+    element.classList.remove('red');
+    element.classList.add('blue');
+  });
+}
+
+function makeOneBoxGreen(){
+  document.getElementById('third_box').classList.add('green');
+}
+
+function resetBoxes(){
+  document.querySelectorAll('.demo_box').forEach((element)=>{
+    element.className = 'demo_box';
+  });
 }
